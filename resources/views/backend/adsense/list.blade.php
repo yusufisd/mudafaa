89c5f84c3fe976a110d747b0bf0820a91ab0633f
yusuf ishak damar <ysfishak14@gmail.com>
2023-12-22 14:07:25 +0300

    adsense bugfix

--- resources/views/backend/adsense/list.blade.php
+++ resources/views/backend/adsense/list.blade.php
@@ -131,22 +131,6 @@
          function change_status(d) {
             window.location.href = "{{ route('admin.adsense.change_status') }}/" + d;
         }
-    </script>
-    <script>var hostUrl = "../assets/";</script>
-    <!--begin::Global Javascript Bundle(mandatory for all pages)-->
-    <script src="../assets/plugins/global/plugins.bundle.js"></script>
-    <script src="../assets/js/scripts.bundle.js"></script>
-    <!--end::Global Javascript Bundle-->
-    <!--begin::Vendors Javascript(used for this page only)-->
-    <script src="../assets/plugins/custom/datatables/datatables.bundle.js"></script>
-    <!--end::Vendors Javascript-->
-    <!--begin::Custom Javascript(used for this page only)-->
-    <script src="../assets/js/widgets.bundle.js"></script>
-    <script src="../assets/js/custom/widgets.js"></script>
-    <!--end::Custom Javascript-->
-    <!--end::Javascript-->
-    <!--begin:: extra js-->
-    <script>
     // begin: DataTable Scripts
         $("#ad_table").DataTable({
             "ordering": true,
